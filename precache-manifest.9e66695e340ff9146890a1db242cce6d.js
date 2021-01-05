d284db815e654e943eaef810668ca372c2d66f6d
unknown <hkymera2@gmail.com>
2021-01-05 20:00:42 +0200

    Updates

---
+++ precache-manifest.9e66695e340ff9146890a1db242cce6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf6b33adf0f98ac600b5de474a012d5",
+    "url": "/vtuberhome/index.html"
+  },
+  {
+    "revision": "fd920aa16fb4119eb8f3",
+    "url": "/vtuberhome/static/css/main.b97e889c.chunk.css"
+  },
+  {
+    "revision": "cbc500474b3587103401",
+    "url": "/vtuberhome/static/js/2.afe523e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vtuberhome/static/js/2.afe523e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd920aa16fb4119eb8f3",
+    "url": "/vtuberhome/static/js/main.cd960d97.chunk.js"
+  },
+  {
+    "revision": "b862ff4f209d679bfaa8",
+    "url": "/vtuberhome/static/js/runtime-main.a4a38c0e.js"
+  },
+  {
+    "revision": "97864e81642c46feeaeca00902b2cd34",
+    "url": "/vtuberhome/static/media/VtuberHomeLogo1.97864e81.png"
+  }
+]);
